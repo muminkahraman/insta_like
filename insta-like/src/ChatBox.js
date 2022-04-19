4ae818e8bf464cf4382fe9d4ce9c53dcf73ebee1
Acedia0 <abdurahim.memic@efrei.net>
2022-04-19 17:57:15 +0100

    CSS + ChatBox

--- insta-like/src/ChatBox.js
+++ insta-like/src/ChatBox.js
@@ -1,50 +1,106 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import "./ChatBox.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
+import './ChatList.css'
 
-export default function AlertDialog() {
-  const [open, setOpen] = React.useState(false);
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  chatSection: {
+    width: '100%',
+    height: '80vh'
+  },
+  headBG: {
+      backgroundColor: '#e0e0e0'
+  },
+  borderRight500: {
+      borderRight: '1px solid #e0e0e0'
+  },
+  messageArea: {
+    height: '70vh',
+    overflowY: 'auto'
+  }
+});
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+const Chat = () => {
+  const classes = useStyles();
 
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Disagree
-          </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+      <div>
+        <Grid container>
+            <Grid item xs={12} >
+                <Typography variant="h5" className="header-message">Chat</Typography>
+            </Grid>
+        </Grid>
+        <Grid container component={Paper} className={classes.chatSection}>
+            <Grid item xs={3} className={classes.borderRight500}>
+                <List>
+                    <ListItem button key="RemySharp">
+                        <ListItemIcon>
+                        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                        </ListItemIcon>
+                        <ListItemText primary="John Wick"></ListItemText>
+                    </ListItem>
+                </List>
+            </Grid>
+            <Grid item xs={9}>
+                <List className={classes.messageArea}>
+                    <ListItem key="1">
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ListItemText align="right" secondary="09:30"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem key="2">
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ListItemText align="left" secondary="09:31"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem key="3">
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <ListItemText align="right" secondary="10:30"></ListItemText>
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                </List>
+                <Divider />
+                <Grid container style={{padding: '20px'}}>
+                    <Grid item xs={11}>
+                        <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+                    </Grid>
+                    <Grid xs={1} align="right">
+                        <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+      </div>
   );
 }
+
+export default Chat;
